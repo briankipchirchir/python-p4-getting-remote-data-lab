a89ee094c5afc95d954783c445f1da043d8b3da8
briankipchirchir <kbrianngetich@gmail.com>
2024-10-03 17:15:45 +0300

    kk

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,17 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            return response.content
+        return None
 
     def load_json(self):
-        pass
+        body = self.get_response_body()
+        if body:
+            return json.loads(body.decode("utf-8"))  # Decoding bytes to string and loading JSON
+        return None
